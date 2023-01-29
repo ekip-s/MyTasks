c36f2dd6e6713d2c8dfc69211c60ea4773e3d476
ekip-s <F6900921f>
2023-01-29 23:39:46 +0300

    docs: Описание первоначального плана.

--- src/main/java/org/example/MyTasks.java
+++ src/main/java/org/example/MyTasks.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyTasks {
-
     public static void main(String[] args) {
         SpringApplication.run(MyTasks.class, args);
     }
